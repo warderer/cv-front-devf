f1e21f221da38350001e40336e55bde355a99d61
César Guerra <warderer@gmail.com>
2020-04-17 01:01:44 -0500

    Profile Form Hardcoded 2: Academic info

--- src/ProfileForm.js
+++ src/ProfileForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Form, FormInput, FormGroup, Container, Row, Col, FormSelect } from "shards-react";
+import { Form, FormInput, FormGroup, Container, Row, Col, FormSelect, FormTextarea } from "shards-react";
 import Header from "./Header";
 
 
@@ -149,7 +149,12 @@ function ProfileForm() {
                             </FormGroup>
     
                             <h2>Información Personal</h2>
+                            <FormGroup>
+                                    <label>Biografía</label>
+                                    <FormTextarea id="about" placeholder="Hablanos un poco acerca de tí" onChange={onChangeInput} />
+                            </FormGroup>
                             
+
                             <FormGroup>
                             <label>Genero</label>
                                 <FormSelect id="genero" onChange={onChangeInput}>
@@ -164,6 +169,132 @@ function ProfileForm() {
                             </FormGroup>
 
                             
+                            {/* HARDCODED++ FALTA AÑADIR VALUE A PARTIR DE AQUI */}
+                            <FormGroup>                    
+                                <label>Teléfono</label>
+                                <FormInput type="tel" id="telefono" placeholder="Número telefonico" onChange={onChangeInput} />
+                            </FormGroup>
+
+                            <FormGroup>                    
+                                <label>Fotografía</label>
+                                <FormInput type="url" id="foto" placeholder="URL de la fotografía" onChange={onChangeInput} />
+                            </FormGroup>
+
+                            <h4>Domicilio</h4>
+
+                            <FormGroup>                    
+                                <label>Calle</label>
+                                <FormInput type="text" id="calle" placeholder="Nombre de la Calle" onChange={onChangeInput} />
+                            </FormGroup>
+
+                            <FormGroup>                    
+                                <label>Número</label>
+                                <FormInput type="number" id="numero" placeholder="Número exterior" onChange={onChangeInput} />
+                            </FormGroup>
+
+                            <FormGroup>                    
+                                <label>Colonia</label>
+                                <FormInput type="text" id="colonia" placeholder="¿En que colonia vives?" onChange={onChangeInput} />
+                            </FormGroup>
+
+                            <FormGroup>                    
+                                <label>C.P.</label>
+                                <FormInput type="text" id="cp" placeholder="¿Cuál es tu número postal?" onChange={onChangeInput} />
+                            </FormGroup>
+
+                            <FormGroup>                    
+                                <label>Ciudad</label>
+                                <FormInput type="text" id="ciudad" placeholder="Ciudad" onChange={onChangeInput} />
+                            </FormGroup>
+
+                            <FormGroup>
+                            <label>Estado</label>
+                                <FormSelect id="genero" onChange={onChangeInput}>
+                                    <option value="no">Seleccione uno...</option>
+                                    <option value="Aguascalientes">Aguascalientes</option>
+                                    <option value="Baja California">Baja California</option>
+                                    <option value="Baja California Sur">Baja California Sur</option>
+                                    <option value="Campeche">Campeche</option>
+                                    <option value="Chiapas">Chiapas</option>
+                                    <option value="Chihuahua">Chihuahua</option>
+                                    <option value="Coahuila">Coahuila</option>
+                                    <option value="Colima">Colima</option>
+                                    <option value="Distrito Federal">Distrito Federal</option>
+                                    <option value="Durango">Durango</option>
+                                    <option value="Estado de México">Estado de México</option>
+                                    <option value="Guanajuato">Guanajuato</option>
+                                    <option value="Guerrero">Guerrero</option>
+                                    <option value="Hidalgo">Hidalgo</option>
+                                    <option value="Jalisco">Jalisco</option>
+                                    <option value="Michoacán">Michoacán</option>
+                                    <option value="Morelos">Morelos</option>
+                                    <option value="Nayarit">Nayarit</option>
+                                    <option value="Nuevo León">Nuevo León</option>
+                                    <option value="Oaxaca">Oaxaca</option>
+                                    <option value="Puebla">Puebla</option>
+                                    <option value="Querétaro">Querétaro</option>
+                                    <option value="Quintana Roo">Quintana Roo</option>
+                                    <option value="San Luis Potosí">San Luis Potosí</option>
+                                    <option value="Sinaloa">Sinaloa</option>
+                                    <option value="Sonora">Sonora</option>
+                                    <option value="Tabasco">Tabasco</option>
+                                    <option value="Tamaulipas">Tamaulipas</option>
+                                    <option value="Tlaxcala">Tlaxcala</option>
+                                    <option value="Veracruz">Veracruz</option>
+                                    <option value="Yucatán">Yucatán</option>
+                                    <option value="Zacatecas">Zacatecas</option>
+                                </FormSelect>
+                            </FormGroup>
+
+                            <h4>Perfiles Sociales</h4>
+                            <FormGroup>                    
+                                <label>Facebook</label>
+                                <FormInput type="url" id="facebook" placeholder="URL de Facebook" onChange={onChangeInput} />
+                            </FormGroup>
+                            <FormGroup>                    
+                                <label>Twitter</label>
+                                <FormInput type="url" id="twitter" placeholder="URL de Twitter" onChange={onChangeInput} />
+                            </FormGroup>
+                            <FormGroup>                    
+                                <label>Linkedin</label>
+                                <FormInput type="url" id="linkedin" placeholder="URL de Linkedin" onChange={onChangeInput} />
+                            </FormGroup>
+                            <FormGroup>                    
+                                <label>Instagram</label>
+                                <FormInput type="url" id="instagram" placeholder="URL de Instagram" onChange={onChangeInput} />
+                            </FormGroup>
+
+                            <h2>Información Académica</h2>
+                            <FormGroup>                    
+                                <label>Escuela</label>
+                                <FormInput type="text" id="escuela" placeholder="Nombre de la escuela" onChange={onChangeInput} />
+                            </FormGroup>
+                            
+                            {/* AQUI VARIE EL ID DE CIUDAD,ESTADO,PAIS; POR QUE ESTOS SON DE LA ESCUELA Y NO DE LA PERSONA*/}
+                            <FormGroup>                    
+                                <label>Ciudad</label>
+                                <FormInput type="text" id="escuelaCiudad" placeholder="Ciudad de la escuela" onChange={onChangeInput} />
+                            </FormGroup>
+                            <FormGroup>                    
+                                <label>Estado</label>
+                                <FormInput type="text" id="escuelaEstado" placeholder="Entidad Federativa de la Escuela" onChange={onChangeInput} />
+                            </FormGroup>
+
+                            <FormGroup>                    
+                                <label>Fecha de Inicio</label>
+                                <FormInput type="date" id="EscuelaInicio" placeholder="¿En que Fecha Iniciaste?" onChange={onChangeInput}/>
+                            </FormGroup>
+
+                            <FormGroup>                    
+                                <label>Fecha de Nacimiento</label>
+                                <FormInput type="date" id="EscuelaTermino" placeholder="¿En que Fecha Terminaste?" onChange={onChangeInput}/>
+                            </FormGroup>
+
+                            <FormGroup>                    
+                                <label>Grado Obtenido</label>
+                                <FormInput type="text" id="escuelaGrado" placeholder="¿Que grado o titulo obtuviste?" onChange={onChangeInput} />
+                            </FormGroup>
+
                         </Form>
                     </Col>
                     <Col>
